f354d4807b84349bb5032559f13fcf2101fd256a
heganghudanni@sina.com <heganghudani>
2020-02-28 08:23:06 +0800

    看nba去了1

--- src/main/java/Test8.java
+++ src/main/java/Test8.java
@@ -3,6 +3,6 @@ public class Test8 {
         System.out.println("我讨厌学git真的好烦，问题多");
         System.out.println("貌似又好了");
         System.out.println("我好像掌握了1");
-        System.out.println("改变2");
+        System.out.println("改变3");
     }
 }
