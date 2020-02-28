d60e52080a423b8c842de98396ee6f910579c42c
heganghudanni@sina.com <heganghudani>
2020-02-28 09:59:35 +0800

    学个锤子

--- src/main/java/Test8.java
+++ src/main/java/Test8.java
@@ -2,5 +2,6 @@ public class Test8 {
     public static void main(String[] args) {
         System.out.println("我讨厌学git真的好烦，问题多");
         System.out.println("貌似又好了");
+        System.out.println("我想提交代码");
     }
 }
