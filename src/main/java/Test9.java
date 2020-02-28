d88de5daf4aa1fa944b32ee522cfb3441681dc2f
heganghudanni@sina.com <heganghudani>
2020-02-28 08:15:27 +0800

    那就这样把

--- src/main/java/Test9.java
+++ src/main/java/Test9.java
@@ -2,5 +2,6 @@ public class Test9 {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("再来一遍就好");
+        System.out.println("改变");
     }
 }
